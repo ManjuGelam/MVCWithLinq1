162f1f8cf65a30d80c7bf5ca2f8fbef82ab5e70d
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 17:47:26 +0530

    Add project files.

---
+++ MVCWithLinq1/Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using MVCWithLinq1.Models;
+
+
+namespace MVCWithLinq1.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDAL obj = new StudentDAL();
+        public ViewResult DisplayStudents()
+        {
+            return View(obj.GetStudents(true));
+        }
+        public ViewResult DisplayStudent(int Sid)
+        {
+            return View(obj.GetStudent(Sid, true));
+        }
+        [HttpGet]
+        public ViewResult AddStudent()
+        {
+            return View(new Student());
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddStudent(Student student, HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                //Checking whether the folder "Uploads" is exists or not and creating it if not exists
+                string PhysicalPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(PhysicalPath))
+                {
+                    Directory.CreateDirectory(PhysicalPath);
+                }
+                selectedFile.SaveAs(PhysicalPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+            }
+            student.Status = true;
+            obj.AddStudent(student);
+            return RedirectToAction("DisplayStudents");
+        }
+        public ViewResult EditStudent(int Sid)
+        {
+            Student student = obj.GetStudent(Sid, true);
+            TempData["Photo"] = student.Photo;
+            return View(student);
+        }
+        public RedirectToRouteResult UpdateStudent(Student student, HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string PhysicalPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(PhysicalPath))
+                {
+                    Directory.CreateDirectory(PhysicalPath);
+                }
+                selectedFile.SaveAs(PhysicalPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+            }
+            else if (TempData["Photo"] != null)
+            {
+                student.Photo = TempData["Photo"].ToString();
+            }
+            obj.UpdateStudent(student);
+            return RedirectToAction("DisplayStudents");
+        }
+        public RedirectToRouteResult DeleteStudent(int Sid)
+        {
+            obj.DeleteStudent(Sid);
+            return RedirectToAction("DisplayStudents");
+        }
+
+    }
+}
---
+++ MVCWithLinq1/Models/StudentDAL.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithLinq1.Models
+{
+    public class StudentDAL
+    {
+        MVCDBDataContext context = new MVCDBDataContext();
+        public List<Student> GetStudents(bool? Status)
+        {
+            List<Student> students;
+            try
+            {
+                if (Status != null)
+                    students = (from s in context.Students where s.Status == Status select s).ToList();
+                else
+                    students = context.Students.ToList();
+                return students;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public Student GetStudent(int Sid, bool? Status)
+        {
+            Student student;
+            try
+            {
+                if (Status == null)
+                    student = (from s in context.Students where s.Sid == Sid select s).Single();
+                else
+                    student = (from s in context.Students where s.Sid == Sid && s.Status == Status select s).Single();
+                return student;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void AddStudent(Student student)
+        {
+            try
+            {
+                context.Students.InsertOnSubmit(student);
+                context.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void UpdateStudent(Student newValues)
+        {
+            try
+            {
+                Student oldValues = context.Students.Single(S => S.Sid == newValues.Sid);
+                oldValues.Name = newValues.Name;
+                oldValues.Class = newValues.Class;
+                oldValues.Fees = newValues.Fees;
+                oldValues.Photo = newValues.Photo;
+                context.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void DeleteStudent(int Sid)
+        {
+            try
+            {
+                Student oldValues = context.Students.First(S => S.Sid == Sid);
+                //dc.Students.DeleteOnSubmit(oldValues); //Permenant Deletion 
+                oldValues.Status = false; //Updates the status with-out deleting the record
+                context.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
